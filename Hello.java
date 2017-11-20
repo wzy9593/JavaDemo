a7ae35612801b71a708931a3e8045356d6773e5a
Zhenyin Wu <wzy9593@brandeis.edu>
2017-11-20 10:34:35 -0500

    add a method

--- Hello.java
+++ Hello.java
@@ -3,5 +3,11 @@ public class Hello {
   public static void main(String[] args){
 
     System.out.println("hello world!");
+    printJapanese();
   }
+public static void printJapanese(){
+  System.out.println("山")
+}
+
+
 }
